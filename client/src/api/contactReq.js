3d1b7bbd91c3445cda37e4af93a0e9efa6b96be0
Agnish Ghosh <iamagnix@gmail.com>
2022-08-24 17:12:12 +0530

    ssh

--- client/src/api/contactReq.js
+++ client/src/api/contactReq.js
@@ -1,17 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseUrl = process.env.REACT_APP_SERVER_URL || 'http://localhost:4000';
+const baseUrl = process.env.REACT_APP_SERVER_URL || "http://localhost:4000";
 
 export const sendContactMessage = async (data) => {
-    try
-    {
-        const res = await axios.post(`${baseUrl}/contact`, data);
-        return res.data;
-    }
-    catch(error)
-    {
-        const res = error.response;
-        console.error(error.message);
-        return { ...res.data, error: "Something went wrong. Please try again." };
-    }
-}
+  try {
+    const res = await axios.post(`${baseUrl}/contact`, data);
+    return res.data;
+  } catch (error) {
+    const res = error.response;
+    console.error(error.message);
+    return { ...res.data, error: "Something went wrong. Please try again." };
+  }
+};
